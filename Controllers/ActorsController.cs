5cb7704e3930873fd967a01fd4371012cf0f3a5c
Mevra Dilan Erten Özbunar <112767383+mevradilanozbunar@users.noreply.github.com>
2023-01-22 20:47:48 +0300

    Proje dosyası ekle.

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IActorsService _service;
+
+        public ActorsController(IActorsService service)
+        {
+            _service = service;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+           var data = await _service.GetAll();
+            return View(data);
+        }
+    }
+}
